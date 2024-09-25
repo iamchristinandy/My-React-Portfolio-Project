4755a63ba9e661ff14ade2cc75796ab11f4f0230
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-09-25 15:27:53 +0000

    Setting up my maiden react portfolio project

---
+++ src/views/Index.js
@@ -0,0 +1,26 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+class Index extends React.Component {
+  render() {
+    return (<h1>Hi there. This is Andrews, and this project is my new React project</h1>);
+  }
+}
+
+export default Index;
