01c4f885404c3fd05d10f5a675fe437089e13d63
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-10-08 13:11:27 +0000

    Creating component for cards

--- src/components/ThreeCards/ThreeCards.js
+++ src/components/ThreeCards/ThreeCards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Badge, Button, Card, CardBody, Col, Container, Row } from 'reactstrap'
+import SimpleCards from '../Cards/SimpleCards'
 
 function ThreeCards() {
   return (
@@ -9,40 +10,7 @@ function ThreeCards() {
         <Col lg="12">
           <Row className="row-grid">
             <Col lg="4">
-              <Card className="card-lift--hover shadow border-0">
-                <CardBody className="py-5">
-                  <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
-                    <i className="ni ni-check-bold" />
-                  </div>
-                  <h6 className="text-primary text-uppercase">
-                    Download Argon
-                  </h6>
-                  <p className="description mt-3">
-                    Argon is a great free UI package based on Bootstrap
-                    4 that includes the most important components and
-                    features.
-                  </p>
-                  <div>
-                    <Badge color="primary" pill className="mr-1">
-                      design
-                    </Badge>
-                    <Badge color="primary" pill className="mr-1">
-                      system
-                    </Badge>
-                    <Badge color="primary" pill className="mr-1">
-                      creative
-                    </Badge>
-                  </div>
-                  <Button
-                    className="mt-4"
-                    color="primary"
-                    href="#pablo"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Learn more
-                  </Button>
-                </CardBody>
-              </Card>
+              <SimpleCards />
             </Col>
             <Col lg="4">
               <Card className="card-lift--hover shadow border-0">
