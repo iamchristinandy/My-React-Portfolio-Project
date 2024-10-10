7e07a716a780d6dac71148721b2ce2dd3bd0825e
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-10-10 15:17:54 +0000

    Fixing bug of profile page link not reacheable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,4 @@
-/*!
-
-=========================================================
-* Argon Design System React - v1.1.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-design-system-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
+import React from "react"; 
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
@@ -31,9 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" exact element={<Index />} />
+      <Route path="/" element={<Index />} />
+      <Route path="/profile" element={<Profile />} />
       <Route path="*" element={<Navigate to="/" replace />} />
-      <Route path="/profile" exact render = {(props) => <Profile {...props}/>} />
     </Routes>
   </BrowserRouter>
 );
