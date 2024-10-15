40b3197328b3498bb9d14d15122817827e4de6db
iamchristinandy <145363456+iamchristinandy@users.noreply.github.com>
2024-10-15 14:10:18 +0000

    Commiting to git for Testimonial.js file to get tracked

---
+++ src/components/Testimonials/Testimonial.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Reactstrap components
+
+import {Button, CardTitle, Container, Row, Col, Carousel, CarouselItem} from 'reactstrap;'
+
+// Core components
+const items = [
+    {
+        content: (
+            <Container>
+                <Row>
+                    <Col classname='ml-auto' md='5'>
+                        <CardTitle tag='h3'>Andrews Baah Kwafo</CardTitle>
+
+                    </Col>
+                </Row>
+            </Container>
+        )
+    }
+]
